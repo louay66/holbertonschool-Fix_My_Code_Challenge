ff46ea6dca6174ed824bb7d7c4bc3ca8b1f4d252
louay66 <louaylouay20@gmail.com>
2022-09-28 10:48:34 +0100

    test

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
+"""class square
+    """
+
 
 class square():
-    
+    """ Documentation """
     width = 0
     height = 0
 
-    
     def __init__(self, *args, **kwargs):
+        """ Documentation """
         for key, value in kwargs.items():
             setattr(self, key, value)
 
@@ -15,10 +18,13 @@ class square():
         return self.width * self.height
 
     def PermiterOfMySquare(self):
+        """ Documentation """
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
+        """ Documentation """
         return "{}/{}".format(self.width, self.height)
+
 
 if __name__ == "__main__":
 
